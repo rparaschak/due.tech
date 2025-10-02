3cec5afe63f83a9a18ed9be25af35326a1a228f5
rparaschak <roman.paraschak@gmail.com>
2025-10-02 14:13:48 +0200

    commented header navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,32 +2,32 @@ import { getBlogPermalink, getAsset, getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    {
-      text: 'Services',
-      links: [
-        {
-          text: 'Supercharge Engineering Team with AI',
-          href: getPermalink('/services/supercharge-engineering-team-with-ai'),
-        },
-        {
-          text: 'Technical Due Diligence',
-          href: getPermalink('/services/technical-due-diligence'),
-        },
-      ],
-    },
-    {
-      text: 'Presentations',
-      links: [
-        {
-          text: 'Coding Agents 101: Foundation and Mindset',
-          href: getPermalink('/presentations/coding-agents-101'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      href: getBlogPermalink(),
-    },
+    // {
+    //   text: 'Services',
+    //   links: [
+    //     {
+    //       text: 'Supercharge Engineering Team with AI',
+    //       href: getPermalink('/services/supercharge-engineering-team-with-ai'),
+    //     },
+    //     {
+    //       text: 'Technical Due Diligence',
+    //       href: getPermalink('/services/technical-due-diligence'),
+    //     },
+    //   ],
+    // },
+    // {
+    //   text: 'Presentations',
+    //   links: [
+    //     {
+    //       text: 'Coding Agents 101: Foundation and Mindset',
+    //       href: getPermalink('/presentations/coding-agents-101'),
+    //     },
+    //   ],
+    // },
+    // {
+    //   text: 'Blog',
+    //   href: getBlogPermalink(),
+    // },
   ],
   actions: [],
 };
